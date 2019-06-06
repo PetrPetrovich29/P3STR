5a715965c3648758f3d70cc07a7dfdc3b9927735
PetrPetrovich29 <petrovichpy29@gmail.com>
2019-06-06 20:26:08 +0300

    Create Fourth.py
    Fourth

---
+++ Fourth.py
@@ -0,0 +1,23 @@
+from turtle import *
+
+speed(90)
+
+for i in range(50):
+	for i in range(4):
+		fd(100)
+		left(90)
+		fd(100)
+		left(90)
+		fd(100)
+		left(90)
+		fd(100)
+		goto(0,0)
+		left(180)
+	goto(0,0)
+	left(19)
+
+
+
+
+
+exitonclick()
